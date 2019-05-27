e10c6fefeb6e48f7eed1583d4343257d2bef4a75
alex.fan <alex.fan@okchem.com>
2019-05-27 17:54:18 +0800

    config for runing config locally

---
+++ config/lighthouse.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  extends: "lighthouse:default",
+  settings: {
+    onlyAudits: [
+      "first-meaningful-paint"
+      // 'speed-index-metric',
+      // 'estimated-input-latency',
+      // 'first-interactive',
+      // 'consistently-interactive',
+    ]
+  }
+};
